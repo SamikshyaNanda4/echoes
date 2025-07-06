008abc4249de72a04f81ecd603f09b4e10f3fe78
samikshyananda4 <samikshya.nanda.4848@gmail.com>
2025-07-06 19:16:30 +0530

    02 Database

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
 import { Button } from "@/components/ui/button";
-
+// import { prisma } from "@/lib/db"
 const Page = () => {
+  // const posts = await prisma.post.findMany();
   return (
     <>
       <h1 className="font-bold text-rose-600">Hello World! Duh</h1>
       <Button variant="outline">Subscribe</Button>
+
+      <div>
+        Hello Page</div>
     </>)
 }
 
