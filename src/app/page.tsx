76924ad9da4c8dba9714d2a69c663c0d5467f7be
samikshyananda4 <samikshya.nanda.4848@gmail.com>
2025-07-08 08:26:11 +0530

    04: Background jobs via inngest

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,32 +1,24 @@
-// "use client"
-// import { Button } from "@/components/ui/button";
-// import { useTRPC } from "@/trpc/client";
-// import { useQuery } from "@tanstack/react-query";
-// import { use } from "react";
-// import { prisma } from "@/lib/db"
-import { Suspense } from "react";
-import { caller, getQueryClient } from "@/trpc/server";
-import { dehydrate, HydrationBoundary, QueryClientContext } from "@tanstack/react-query";
-import { trpc } from "@/trpc/server";
-import { Client } from "./client";
+"use client"
 
-// import { da } from "date-fns/locale";
-const Page = async () => {
-  // const trpc = useTRPC();
-  // const { data } = useQuery(trpc.createAI.queryOptions({ text: "Samikshya Nanda!", number: 45 }))
-  // console.log("Samikshya")
-  // const posts = await prisma.post.findMany();
-  //localhost:3000/api/create-ai/?body={t}
-  const queryClient = getQueryClient();
-  void queryClient.prefetchQuery(trpc.createAI.queryOptions({ text: "Samikshya PREFETCH", number: 231 }))
-  // const data = await caller.createAI({ text: "Samikshya Nanda", number: 23 })
+import { Button } from "@/components/ui/button"
+import { useTRPC } from "@/trpc/client"
+import { useMutation } from "@tanstack/react-query";
+import { toast } from "sonner";
+
+const Page = () => {
+  const trpc = useTRPC();
+  const invoke = useMutation(trpc.invoke.mutationOptions({
+    onSuccess: () => {
+      toast.success("Background Job Started...")
+    }
+  }))
+
   return (
-    <HydrationBoundary state={dehydrate(queryClient)}>
-      <Suspense fallback={<p>Loading</p>}>
-        <Client />
-      </Suspense>
-    </HydrationBoundary>
+    <div className="p-4 max-w-7xl mx-auto">
+      <Button disabled={invoke.isPending} variant="default" onClick={() => invoke.mutate({ text: "sam@gmail.com" })} className="cursor-pointer">
+        Invoke Background Job
+      </Button>
+    </div>
   )
 }
-
-export default Page;
+export default Page
